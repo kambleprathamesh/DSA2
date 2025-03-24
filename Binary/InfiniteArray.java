c8c880ecbe129179820d2695d1b091bf9f259553
prathmesh <prathameshk990@gmail.com>
2025-03-25 02:25:47 +0530

    done InfiniteArray

--- Binary/InfiniteArray.java
+++ Binary/InfiniteArray.java
@@ -1,24 +1,64 @@
+// package Binary;
+
+// public class InfiniteArray {
+//     public static void main(String[] args) {
+
+//     }
+
+//     static int answer(int arr[], int target) {
+//         int key = 0;
+//         int start = 0;
+//         int end = 1;
+//         while (target >= arr[end]) {
+//             int newStart = end + 1;
+//             end = end - start + 1;
+//             start = newStart;
+//         }
+//         return SearchArray(arr, target, start, end);
+//     }
+
+//     static int SearchArray(int arr[], int target, int start, int end) {
+//         int key = 0;
+//         return key;
+//     }
+// }
+
 package Binary;
 
 public class InfiniteArray {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        int arr[] = { 11,12,13,15,16,17,18,19,20,21,30,40,60,90,100,111,11,113,114,115,116,117,118 };
 
+        System.out.println("KEY " + range(arr, 118));
     }
 
-    static int answer(int arr[], int target) {
-        int key = 0;
+    static int range(int arr[], int target) {
         int start = 0;
         int end = 1;
+
         while (target >= arr[end]) {
-            int newStart = end + 1;
-            end = end - start + 1;
-            start = newStart;
+            int newstart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newstart;
+
         }
-        return SearchArray(arr, target, start, end);
+
+        return searchArray(arr, target, start, end);
+
     }
 
-    static int SearchArray(int arr[], int target, int start, int end) {
-        int key = 0;
-        return key;
+    static int searchArray(int arr[], int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
+
 }
